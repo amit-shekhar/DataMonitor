861ecca810af44dd2b9e31483781c021ce60b760
amit.shekhar <amit.shekhar@box8.in>
2016-11-12 22:38:00 +0530

    yet to configure response correctly

--- config/secrets.js
+++ config/secrets.js
@@ -1,6 +1,12 @@
 var config = {}
 
 config.PORT = process.env.PORT || 8080;
-config.API_KEY = "https://thawing-refuge-25082.herokuapp.com/"
+config.API_URL = "https://thawing-refuge-25082.herokuapp.com/";
+config.USERNAME = '';
+config.PASSWORD = '';
+config.GMAIL_USERNAME =  'aisforamit@gmail.com';
+config.GMAIL_PASSWORD =  'iloveitaly';
+config.MAIL_FROM = 'amit.shekhar@box8.in';
+config.MAIL_TO =  'aisforamit@gmail.com';
 
 module.exports = config;
--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -2,15 +2,36 @@
 
 angular.module("DataMonitor")
 .controller("MainCtrl",function($scope,$http,$interval){
-  $scope.name = "data table";
+  $scope.name = "Data Monitor";
 
-  $interval(function(){
+  function getData(){
     $http({
       method: "GET",
       url: "/data"
     }).then(function(response){
-      $scope.data = response;
+      $scope.docs = response.data.response.docs;
     }); 
-  },1000*60);
+  }
+
+  getData();
+
+  $interval(function(){
+    getData();
+  },1000*15);
+
+  
+
+
+  $scope.get_count = function(model,fq_bin){
+    return $scope.docs.filter(function(obj){
+     return obj.model === model && obj.fq_bin === fq_bin;
+    }).length;
+  };
+
+  $scope.select_docs = function(model,fq_bin){
+    $scope.selected_docs = $scope.docs.filter(function(obj){
+     return obj.model === model && obj.fq_bin === fq_bin;
+    });
+  };
 
 });
