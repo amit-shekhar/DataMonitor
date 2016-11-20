9525cce42edfdcc11e7c464ca7fda29c7e2e9085
amit.shekhar <amit.shekhar@box8.in>
2016-11-20 20:56:08 +0530

    test mailer setup

--- config/secrets.js
+++ config/secrets.js
@@ -7,10 +7,10 @@ config.USERNAME = "username";
 config.PASSWORD = "password";
 config.MAIL_HOST = "smtp.gmail.com";
 config.MAIL_PORT = 587;
-config.MAIL_USERNAME =  "";
-config.MAIL_PASSWORD =  "";
-config.MAIL_FROM = "";
-config.MAIL_TO =  "test1@gmail.com , mr.profficient@gmail.com , test2@gmail.com";
+config.MAIL_USERNAME =  "1q2w3v@gmail.com";
+config.MAIL_PASSWORD =  "encrypted";
+config.MAIL_FROM = "mailer@test.com";
+config.MAIL_TO =  "test1@gmail.com , test2@gmail.com";
 config.MAIL_FOOTER_TEXT = "put the footer text here";
 
 module.exports = config;
