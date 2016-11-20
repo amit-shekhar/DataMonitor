cc85a955fd885d7a01082a5af5ce9af1b4b446dd
amit.shekhar <amit.shekhar@box8.in>
2016-11-20 19:22:34 +0530

    final fixes

--- config/secrets.js
+++ config/secrets.js
@@ -2,11 +2,15 @@ var config = {}
 
 config.PORT = process.env.PORT || 8080;
 config.API_URL = "https://thawing-refuge-25082.herokuapp.com/";
+config.API_URL_METHOD = "POST";
 config.USERNAME = "username";
 config.PASSWORD = "password";
-config.GMAIL_USERNAME =  "";
-config.GMAIL_PASSWORD =  "";
+config.MAIL_HOST = "smtp.gmail.com";
+config.MAIL_PORT = 587;
+config.MAIL_USERNAME =  "";
+config.MAIL_PASSWORD =  "";
 config.MAIL_FROM = "";
-config.MAIL_TO =  "";
+config.MAIL_TO =  "test1@gmail.com , mr.profficient@gmail.com , test2@gmail.com";
+config.MAIL_FOOTER_TEXT = "put the footer text here";
 
 module.exports = config;
